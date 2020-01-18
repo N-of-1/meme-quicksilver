86eceb2b6c28a7aacca543755f23606de75737f8
Paul Houghton <paulirotta@gmail.com>
2020-01-18 12:56:35 +0200

    Transparent logo

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,43 @@ use quicksilver::{
     sound::Sound,
     Future, Result,
 };
+use std::env;
+
+const IMG_LOGO: &str = "N_of_1_logo_blue_transparent.png";
+
+const FONT_TITLE: &str = "WorkSans-ExtraBold.ttf";
+
+const SND_WELCOME: &str = "moog.ogg";
+const SND_CLICK: &str = "click.ogg";
+const SND_BLAH: &str = "blah.ogg";
 
 const ENV_SCREEN_SIZE: &str = "SCREEN_SIZE";
+
 const STR_TITLE: &str = "Meme Machine";
-const COLOR_GREY: Color = Color {
+
+const CLR_GREY: Color = Color {
     r: 0.5,
     g: 0.5,
     b: 0.5,
     a: 1.0,
 };
-const COLOR_BACKGROUND: Color = COLOR_GREY;
-const COLOR_TEXT: Color = Color::BLACK;
-const COLOR_BUTTON: Color = Color::BLUE;
-const COLOR_BUTTON_PRESSED: Color = Color::WHITE;
+const CLR_CLEAR: Color = Color {
+    r: 0.5,
+    g: 0.5,
+    b: 0.5,
+    a: 0.0,
+};
+const CLR_BACKGROUND: Color = CLR_GREY;
+const CLR_TEXT: Color = Color::BLACK;
+const CLR_BUTTON: Color = Color::BLUE;
+const CLR_BUTTON_PRESSED: Color = Color::WHITE;
 
-const LEFT_BUTTON_AREA: Rectangle = Rectangle {
+const RECT_LEFT_BUTTON: Rectangle = Rectangle {
     pos: Vector { x: 100.0, y: 350.0 },
     size: Vector { x: 100.0, y: 50.0 },
 };
 
-const RIGHT_BUTTON_AREA: Rectangle = Rectangle {
+const RECT_RIGHT_BUTTON: Rectangle = Rectangle {
     pos: Vector { x: 350.0, y: 350.0 },
     size: Vector { x: 100.0, y: 50.0 },
 };
@@ -46,53 +63,70 @@ struct DrawState {
 }
 
 impl DrawState {
-    // Perform any shutdown actions like closing the datbase if the user or OS clos
+    // Perform any shutdown actions
     // Do not call this directly to end the app. Instead call window.close();
-    fn shutdown_hooks() {
-        // Perform any shutdown actions like closing the datbase if the user or OS clos
+    fn shutdown_hooks(&mut self) -> Result<()> {
+        // TODO Notify database session ended
+
+        Ok(())
+    }
+}
+
+impl DrawState {
+    fn left_action(&mut self, _window: &mut Window) -> Result<()> {
+        self.left_button_color = CLR_BUTTON_PRESSED;
+        self.click_sound
+            .execute(|sound| sound.play())
+            .expect("Could not play left button sound");
+        self.blah_sound.execute(|sound| sound.play())
+    }
+
+    fn right_action(&mut self, _window: &mut Window) -> Result<()> {
+        self.right_button_color = CLR_BUTTON_PRESSED;
+        self.click_sound.execute(|sound| sound.play())
     }
 }
 
 impl State for DrawState {
     fn new() -> Result<DrawState> {
-        let extra_bold = Asset::new(Font::load("WorkSans-ExtraBold.ttf").and_then(|font| {
-            let style = FontStyle::new(72.0, COLOR_TEXT);
+        let extra_bold = Asset::new(Font::load(FONT_TITLE).and_then(|font| {
+            let style = FontStyle::new(72.0, CLR_TEXT);
             result(font.render(STR_TITLE, &style))
         }));
 
-        let logo = Asset::new(Image::load("nof1-logo.png"));
-
-        let click_sound = Asset::new(Sound::load("click.ogg"));
-
-        let blah_sound = Asset::new(Sound::load("blah.ogg"));
-
-        let welcome_sound = Asset::new(Sound::load("moog.ogg"));
+        let logo = Asset::new(Image::load(IMG_LOGO));
+        let sound_click = Asset::new(Sound::load(SND_CLICK));
+        let sound_blah = Asset::new(Sound::load(SND_BLAH));
+        let sound_welcome = Asset::new(Sound::load(SND_WELCOME));
 
         Ok(DrawState {
             frame_count: 0,
             extra_bold: extra_bold,
             logo: logo,
-            click_sound: click_sound,
-            blah_sound: blah_sound,
-            welcome_sound: welcome_sound,
-            left_button_color: COLOR_BUTTON,
-            right_button_color: COLOR_BUTTON,
+            click_sound: sound_click,
+            blah_sound: sound_blah,
+            welcome_sound: sound_welcome,
+            left_button_color: CLR_CLEAR,
+            right_button_color: CLR_CLEAR,
         })
     }
 
     // This is called 60 times per second
     fn update(&mut self, window: &mut Window) -> Result<()> {
         // EXIT APP
-        if window.keyboard()[Key::Escape].is_down()
-            || window
-                .gamepads()
-                .iter()
-                .any(|pad| pad[GamepadButton::FaceLeft].is_down())
+        #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
         {
-            window.close();
+            if window.keyboard()[Key::Escape].is_down()
+                || window
+                    .gamepads()
+                    .iter()
+                    .any(|pad| pad[GamepadButton::FaceLeft].is_down())
+            {
+                window.close();
+            }
         }
 
-        // LEFT ACTION
+        // LEFT SHIFT OR GAMEPAD ACTION
         if window.keyboard()[Key::LShift] == ButtonState::Pressed
             || window
                 .gamepads()
@@ -103,16 +137,10 @@ impl State for DrawState {
                 .iter()
                 .any(|pad| pad[GamepadButton::ShoulderLeft].is_down())
         {
-            self.left_button_color = COLOR_BUTTON_PRESSED;
-            self.click_sound
-                .execute(|sound| sound.play())
-                .expect("Could not play left button sound");
-            self.blah_sound
-                .execute(|sound| sound.play())
-                .expect("Could not play left button sound");
+            self.left_action(window)?;
         }
 
-        // RIGHT ACTION
+        // RIGHT SHIFT OR GAMEPAD ACTION
         if window.keyboard()[Key::RShift] == ButtonState::Pressed
             || window
                 .gamepads()
@@ -123,55 +151,51 @@ impl State for DrawState {
                 .iter()
                 .any(|pad| pad[GamepadButton::ShoulderRight].is_down())
         {
-            self.right_button_color = COLOR_BUTTON_PRESSED;
-            self.click_sound
-                .execute(|sound| sound.play())
-                .expect("Could not play right button sound");
+            self.right_action(window)?;
         }
 
         // LEFT SCREEN BUTTON PRESS
         if window.mouse()[MouseButton::Left] == ButtonState::Pressed
-            && LEFT_BUTTON_AREA.contains(window.mouse().pos())
+            && RECT_LEFT_BUTTON.contains(window.mouse().pos())
         {
-            self.left_button_color = COLOR_BUTTON_PRESSED;
-            self.click_sound
-                .execute(|sound| sound.play())
-                .expect("Could not play left button sound");
-            self.blah_sound
-                .execute(|sound| sound.play())
-                .expect("Could not play left button sound");
+            self.left_action(window)?;
         }
 
         // RIGHT SCREEN BUTTON PRESS
         if window.mouse()[MouseButton::Left] == ButtonState::Pressed
-            && RIGHT_BUTTON_AREA.contains(window.mouse().pos())
+            && RECT_RIGHT_BUTTON.contains(window.mouse().pos())
         {
-            self.right_button_color = COLOR_BUTTON_PRESSED;
-            self.click_sound
-                .execute(|sound| sound.play())
-                .expect("Could not play right button sound");
+            self.right_action(window)?;
         }
+
+        // TODO NANO SEEED BUTTON PRESS
 
         Ok(())
     }
 
     fn event(&mut self, event: &Event, _window: &mut Window) -> Result<()> {
         if let Event::Closed = event {
-            // TODO self.shutdown_hooks();
+            self.shutdown_hooks()?;
         }
+
         Ok(())
     }
 
     // This is called 30 times per second
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         if self.frame_count == 0 {
-            //TODO            window.set_size(window.screen_size());
+            let screen_size = window.screen_size();
+            env::set_var(
+                ENV_SCREEN_SIZE,
+                format!("{},{}", screen_size.x, screen_size.y),
+            );
+
             self.welcome_sound
                 .execute(|sound| sound.play())
                 .expect("Could not play right button sound");
         }
 
-        window.clear(COLOR_BACKGROUND)?;
+        window.clear(CLR_BACKGROUND)?;
 
         // LOGO
         self.logo.execute(|image| {
@@ -188,18 +212,18 @@ impl State for DrawState {
         // LEFT BUTTON
         let left_color = self.left_button_color;
         self.click_sound.execute(|_| {
-            window.draw(&LEFT_BUTTON_AREA, Col(left_color));
+            window.draw(&RECT_LEFT_BUTTON, Col(left_color));
             Ok(())
         })?;
-        self.left_button_color = COLOR_BUTTON;
+        self.left_button_color = CLR_BUTTON;
 
         // RIGHT BUTTON
         let right_color = self.right_button_color;
         self.click_sound.execute(|_| {
-            window.draw(&RIGHT_BUTTON_AREA, Col(right_color));
+            window.draw(&RECT_RIGHT_BUTTON, Col(right_color));
             Ok(())
         })?;
-        self.right_button_color = COLOR_BUTTON;
+        self.right_button_color = CLR_BUTTON;
 
         /*        window.draw(&Rectangle::new((100, 100), (32, 32)), Col(Color::BLUE));
         window.draw_ex(
@@ -248,6 +272,19 @@ fn main() {
         vsync: true,
         multisampling: None,
     };*/
+    let mut screen_size = Vector::new(1280, 768);
+    match env::var(ENV_SCREEN_SIZE) {
+        Ok(ss) => {
+            let parsed: Vec<&str> = ss.split(',').collect();
+            screen_size.x = parsed[0]
+                .parse::<f32>()
+                .expect("Local variable for screen size in wrong format 'x,y'");
+            screen_size.y = parsed[1]
+                .parse::<f32>()
+                .expect("Local variable for screen size in wrong format 'x,y'");
+        }
+        _ => (),
+    }
 
     let settings = Settings {
         icon_path: Some("n-icon.png"),
@@ -257,5 +294,7 @@ fn main() {
         ..Settings::default()
     };
 
-    run::<DrawState>("Meme Machine", Vector::new(1280, 768), settings);
+    run::<DrawState>("Meme Machine", screen_size, settings);
 }
+
+//fn get_screen_size() -> Vec
